34b2087446f2041bc399cadb31013df8f6b7ff59
renanmarcilio <marcdevrenan@gmail.com>
2024-04-09 20:36:10 -0300

    refact: refining object-oriented principles

--- main.py
+++ main.py
@@ -11,51 +11,53 @@ import logging
 
 class NewsBot:
     def __init__(self):
-        self.configure_logging()
-        self.initialize_browser()
-        self.results = []
-        self.temp_folder = 'temp_folder'
+        self._configure_logging()
+        self._initialize_browser()
+        self._results = []
+        self._temp_folder = 'temp_folder'
 
-    def configure_logging(self):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.INFO)
-        self.handler = logging.StreamHandler()
-        self.handler.setLevel(logging.INFO)
-        self.formatter = logging.Formatter(
+    def _configure_logging(self):
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
+        self._handler = logging.StreamHandler()
+        self._handler.setLevel(logging.INFO)
+        self._formatter = logging.Formatter(
             '%(asctime)s - %(levelname)s - %(message)s')
-        self.handler.setFormatter(self.formatter)
-        self.logger.addHandler(self.handler)
+        self._handler.setFormatter(self._formatter)
+        self._logger.addHandler(self._handler)
 
-    def initialize_browser(self):
-        self.driver = webdriver.Chrome()
+    def _initialize_browser(self):
+        self._driver = webdriver.Chrome()
 
-    def navigate_to_site(self):
+    def _navigate_to_site(self, url):
         try:
-            self.driver.get("https://news.yahoo.com/")
-            self.logger.info("Navigated to Yahoo! News website")
+            self._driver.get(url)
+            self._logger.info(f"Navigated to {url}")
         except Exception as e:
-            self.logger.error(
-                f"An error occurred while navigating to the website: {
+            self._logger.error(
+                f"An error occurred while navigating to {url}: {
                     str(e)}")
 
-    def search_news(self, search_phrase, category):
-        self.logger.info(
+    def _search_news(self, search_phrase, category):
+        self._logger.info(
             f"Searching for: {search_phrase} in category: {category}")
         try:
-            search_input = self.driver.find_element(
+            search_input = self._driver.find_element(
                 By.CSS_SELECTOR, "input[name='p']")
             search_input.send_keys(search_phrase)
             search_input.submit()
 
-            WebDriverWait(self.driver, 10).until(EC.number_of_windows_to_be(2))
-            self.driver.switch_to.window(self.driver.window_handles[-1])
-            self.logger.info("Switched to new tab")
+            WebDriverWait(
+                self._driver, 10).until(
+                EC.number_of_windows_to_be(2))
+            self._driver.switch_to.window(self._driver.window_handles[-1])
+            self._logger.info("Switched to new tab")
 
-            self.select_category(category)
+            self._select_category(category)
         except Exception as e:
-            self.logger.error(f"An error occurred while searching: {str(e)}")
+            self._logger.error(f"An error occurred while searching: {str(e)}")
 
-    def download_image(self, url, filename):
+    def _download_image(self, url, filename):
         try:
             response = requests.get(url)
             if response.status_code == 200:
@@ -63,35 +65,35 @@ class NewsBot:
                     f.write(response.content)
                 return filename
             else:
-                self.logger.error(
+                self._logger.error(
                     f"Error downloading image: {
                         response.status_code}")
                 return None
         except Exception as e:
-            self.logger.error(f"Error downloading image: {str(e)}")
+            self._logger.error(f"Error downloading image: {str(e)}")
             return None
 
-    def store_results_to_excel(self, search_phrase, category):
+    def _store_results_to_excel(self, search_phrase, category):
         try:
-            df = pd.DataFrame(self.results)
+            df = pd.DataFrame(self._results)
 
             filename = f"{search_phrase}_{category}_results.xlsx"
             df.to_excel(filename, index=False)
-            self.logger.info(f"Results saved to {filename}")
+            self._logger.info(f"Results saved to {filename}")
         except Exception as e:
-            self.logger.error(f"Error storing results: {str(e)}")
+            self._logger.error(f"Error storing results: {str(e)}")
 
-    def select_category(self, category):
+    def _select_category(self, category):
         try:
             category_switch = {
-                'news': self.select_latest_news,
-                'images': self.select_latest_image,
-                'videos': self.select_latest_video,
-                'local': self.select_latest_local,
-                'shopping': self.select_latest_shopping
+                'news': self._select_latest_news,
+                'images': self._select_latest_image,
+                'videos': self._select_latest_video,
+                'local': self._select_latest_local,
+                'shopping': self._select_latest_shopping
             }
 
-            category_links = self.driver.find_elements(
+            category_links = self._driver.find_elements(
                 By.CSS_SELECTOR, "div.dd.horizontal-pivots a.td-n")
             available_categories = [link.text.strip().lower()
                                     for link in category_links]
@@ -100,7 +102,7 @@ class NewsBot:
                 category_link = category_links[available_categories.index(
                     category.lower())]
                 category_link.click()
-                self.logger.info(f"Selected category: {category}")
+                self._logger.info(f"Selected category: {category}")
 
                 selected_category_method = category_switch.get(
                     category.lower())
@@ -108,98 +110,101 @@ class NewsBot:
                     latest_category_elements = selected_category_method()
                     if latest_category_elements:
                         for element in latest_category_elements:
-                            self.extract_category_details(
+                            self._extract_category_details(
                                 category.lower(), element)
                 return
             elif "more" in available_categories:
                 more_link = category_links[available_categories.index("more")]
                 more_link.click()
-                self.logger.info(
+                self._logger.info(
                     "Clicked on 'More' to reveal hidden categories")
-                return self.select_category(category)
+                return self._select_category(category)
             else:
-                self.logger.warning(
+                self._logger.warning(
                     f"Category '{category}' not found. Available categories: {
-                        available_categories}"
+                                     available_categories}"
                 )
         except Exception as e:
-            self.logger.error(f"Error selecting category {category}: {str(e)}")
+            self._logger.error(
+                f"Error selecting category {category}: {
+                    str(e)}"
+            )
 
-    def extract_category_details(self, category, latest_category_element):
+    def _extract_category_details(self, category, latest_category_element):
         if category == 'news':
-            self.extract_news_details(latest_category_element)
+            self._extract_news_details(latest_category_element)
         elif category == 'videos':
-            self.extract_video_details(latest_category_element)
+            self._extract_video_details(latest_category_element)
         elif category == 'images':
-            self.extract_image_details(latest_category_element)
+            self._extract_image_details(latest_category_element)
         elif category == 'local':
-            self.extract_local_details(latest_category_element)
+            self._extract_local_details(latest_category_element)
         elif category == 'shopping':
-            self.extract_shopping_details(latest_category_element)
+            self._extract_shopping_details(latest_category_element)
 
-    def select_latest_news(self):
+    def _select_latest_news(self):
         try:
-            latest_news_elements = self.driver.find_elements(
+            latest_news_elements = self._driver.find_elements(
                 By.CSS_SELECTOR, "div.dd.NewsArticle")
             if latest_news_elements:
-                self.logger.info("Selected the latest news")
+                self._logger.info("Selected the latest news")
                 return latest_news_elements
             else:
-                self.logger.info("No latest news found")
+                self._logger.info("No latest news found")
                 return None
         except Exception as e:
-            self.logger.error(f"Error selecting latest news: {str(e)}")
+            self._logger.error(f"Error selecting latest news: {str(e)}")
             return None
 
-    def extract_news_details(self, latest_news_element):
+    def _extract_news_details(self, latest_news_element):
         try:
             title_element = latest_news_element.find_element(
                 By.CSS_SELECTOR, "h4.s-title a")
             news_title = title_element.text.strip()
-            self.logger.info(f"Title: {news_title}")
+            self._logger.info(f"Title: {news_title}")
 
             source_element = latest_news_element.find_element(
                 By.CSS_SELECTOR, ".s-source")
             news_source = source_element.text.strip()
-            self.logger.info(f"Source: {news_source}")
+            self._logger.info(f"Source: {news_source}")
 
             time_element = latest_news_element.find_element(
                 By.CSS_SELECTOR, ".s-time")
             news_time = time_element.text.strip()
-            self.logger.info(f"Time: {news_time}")
+            self._logger.info(f"Time: {news_time}")
 
             description_element = latest_news_element.find_element(
                 By.CSS_SELECTOR, ".s-desc")
             news_description = description_element.text.strip()
-            self.logger.info(f"Description: {news_description}")
+            self._logger.info(f"Description: {news_description}")
 
             url = title_element.get_attribute("href")
-            self.logger.info(f"URL: {url}")
+            self._logger.info(f"URL: {url}")
 
-            self.results.append({'Title': news_title,
-                                 'Source': news_source,
-                                 'Time': news_time,
-                                 'Description': news_description,
-                                 'URL': url})
+            self._results.append({'Title': news_title,
+                                  'Source': news_source,
+                                  'Time': news_time,
+                                  'Description': news_description,
+                                  'URL': url})
         except Exception as e:
-            self.logger.error(f"Error extracting news details: {str(e)}")
+            self._logger.error(f"Error extracting news details: {str(e)}")
 
-    def select_latest_image(self):
+    def _select_latest_image(self):
         try:
-            image_elements = self.driver.find_elements(
+            image_elements = self._driver.find_elements(
                 By.CSS_SELECTOR, "li.ld.r0")
             if image_elements:
-                self.logger.info("Found images in the latest images section")
+                self._logger.info("Found images in the latest images section")
                 return image_elements
             else:
-                self.logger.info(
+                self._logger.info(
                     "No images found in the latest images section")
                 return None
         except Exception as e:
-            self.logger.error(f"Error selecting latest images: {str(e)}")
+            self._logger.error(f"Error selecting latest images: {str(e)}")
             return None
 
-    def extract_image_details(self, image_element):
+    def _extract_image_details(self, image_element):
         try:
             title_element = image_element.find_element(
                 By.CSS_SELECTOR, "div.img-desc span.title")
@@ -214,37 +219,40 @@ class NewsBot:
 
             image_url = image_element.find_element(
                 By.CSS_SELECTOR, "img").get_attribute("src")
-            image_filename = os.path.join(self.temp_folder, f"{re.sub(
-                '[^\\w\\-_. ]', '_', image_title)}.jpg")
-            if not os.path.exists(self.temp_folder):
-                os.makedirs(self.temp_folder)
-            self.download_image(image_url, image_filename)
+            image_filename = os.path.join(
+                self._temp_folder, f"{
+                    re.sub(
+                        '[^\\w\\-_. ]', '_', image_title)}.jpg"
+            )
+            if not os.path.exists(self._temp_folder):
+                os.makedirs(self._temp_folder)
+            self._download_image(image_url, image_filename)
 
-            self.results.append({
+            self._results.append({
                 'Title': image_title,
                 'URL': url,
                 'Source': image_source,
                 'Image Filename': image_filename
             })
         except Exception as e:
-            self.logger.error(f"Error extracting image details: {str(e)}")
+            self._logger.error(f"Error extracting image details: {str(e)}")
 
-    def select_latest_video(self):
+    def _select_latest_video(self):
         try:
-            video_elements = self.driver.find_elements(
+            video_elements = self._driver.find_elements(
                 By.CSS_SELECTOR, "li.vr.vres")
             if video_elements:
-                self.logger.info("Found videos in the latest videos section")
+                self._logger.info("Found videos in the latest videos section")
                 return video_elements
             else:
-                self.logger.info(
+                self._logger.info(
                     "No videos found in the latest videos section")
                 return None
         except Exception as e:
-            self.logger.error(f"Error selecting latest video: {str(e)}")
+            self._logger.error(f"Error selecting latest video: {str(e)}")
             return None
 
-    def extract_video_details(self, video_element):
+    def _extract_video_details(self, video_element):
         try:
             title_element = video_element.find_element(By.CSS_SELECTOR, "h3")
             video_title = title_element.text.strip()
@@ -267,24 +275,27 @@ class NewsBot:
                 'Age': video_age
             }
         except Exception as e:
-            self.logger.error(f"Error extracting video details: {str(e)}")
+            self._logger.error(f"Error extracting video details: {str(e)}")
             return None
 
-    def select_latest_local(self):
+    def _select_latest_local(self):
         try:
-            local_elements = self.driver.find_elements(
+            local_elements = self._driver.find_elements(
                 By.CSS_SELECTOR, "li.list")
             if local_elements:
-                self.logger.info("Found local results")
+                self._logger.info("Found local results")
                 return local_elements
             else:
-                self.logger.info("No local results found")
+                self._logger.info("No local results found")
                 return None
         except Exception as e:
-            self.logger.error(f"Error selecting latest local result: {str(e)}")
+            self._logger.error(
+                f"Error selecting latest local result: {
+                    str(e)}"
+            )
             return None
 
-    def extract_local_details(self, local_element):
+    def _extract_local_details(self, local_element):
         try:
             title_element = local_element.find_element(
                 By.CSS_SELECTOR, "div.titlewrapper a")
@@ -307,27 +318,27 @@ class NewsBot:
                 'Phone': local_phone
             }
         except Exception as e:
-            self.logger.error(f"Error extracting local details: {str(e)}")
+            self._logger.error(f"Error extracting local details: {str(e)}")
             return None
 
-    def select_latest_shopping(self):
+    def _select_latest_shopping(self):
         try:
-            shopping_elements = self.driver.find_elements(
+            shopping_elements = self._driver.find_elements(
                 By.CSS_SELECTOR, "li.Grid__Cell-sc-1xfj9j2-1.exzZYi")
             if shopping_elements:
-                self.logger.info("Found shopping results")
+                self._logger.info("Found shopping results")
                 return shopping_elements
             else:
-                self.logger.info("No shopping results found")
+                self._logger.info("No shopping results found")
                 return None
         except Exception as e:
-            self.logger.error(
+            self._logger.error(
                 f"Error selecting latest shopping result: {
                     str(e)}"
             )
             return None
 
-    def extract_shopping_details(self, shopping_element):
+    def _extract_shopping_details(self, shopping_element):
         try:
             title_element = shopping_element.find_element(
                 By.CSS_SELECTOR,
@@ -355,11 +366,22 @@ class NewsBot:
                 'Merchant': shopping_merchant
             }
         except Exception as e:
-            self.logger.error(f"Error extracting shopping details: {str(e)}")
+            self._logger.error(f"Error extracting shopping details: {str(e)}")
             return None
 
-    def close_driver(self):
-        self.driver.quit()
+    def _close_driver(self):
+        self._driver.quit()
+        logging.shutdown()
+
+    def run(self, search_phrase, category):
+        try:
+            self._navigate_to_site("https://news.yahoo.com/")
+            self._search_news(search_phrase, category)
+            self._store_results_to_excel(search_phrase, category)
+        except Exception as e:
+            self._logger.error(f"An error occurred: {str(e)}")
+        finally:
+            self._close_driver()
 
 
 if __name__ == "__main__":
@@ -367,11 +389,4 @@ if __name__ == "__main__":
     category = input("Enter category: ")
 
     news_bot = NewsBot()
-    try:
-        news_bot.navigate_to_site()
-        news_bot.search_news(search_phrase, category)
-        news_bot.store_results_to_excel(search_phrase, category)
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-    finally:
-        news_bot.close_driver()
+    news_bot.run(search_phrase, category)
